ec0e6a4603b334217875d2a78db26d2b26984f4b
Jorge-Cammarota <Jorge-Cammarota>
2023-09-07 23:52:21 -0300

    casa decimal

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import static java.lang.System.*;
+
+public class Main {
+        public static void main(String[] args) {
+            double x = 10.35784;
+            System.out.println(x);
+            System.out.printf("%.2f%n", x);
+
+                }
+            }
